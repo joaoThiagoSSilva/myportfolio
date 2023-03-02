6a5ef858dc52b7d052ab6b6fdfa0552e6d6b32dc
João Thiago Souza Silva <106702402+joaoThiagoSSilva@users.noreply.github.com>
2023-03-02 16:22:04 -0300

    project almost done

---
+++ src/utils/stackData.ts
@@ -0,0 +1,33 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiPostgresql, SiTypescript } from "react-icons/si";
+import { FaGit, FaPython } from "react-icons/fa";
+import {
+  FaDocker,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNode,
+  FaReact,
+} from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "JS",
+    img: FaJs,
+  },
+  { title: "TypeScript", img: SiTypescript },
+
+  {
+    title: "React",
+    img: FaReact,
+  },
+  { title: "Git", img: FaGit },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,8 @@
+export const userData = {
+  nameUser: "João Thiago",
+  githubUser: "joaoThiagoSSilva",
+  linkedinUser: "joão-thiago-souza-silva",
+  whatsappNumber: "12991736012",
+  instagramUser: "jowthiagu",
+  facebookUser: "",
+};
